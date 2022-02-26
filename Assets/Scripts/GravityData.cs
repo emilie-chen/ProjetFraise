90c2829c3ce443118ab996b3add1a812e6a18636
Hongjun Chen <hongjun.chen2@mail.mcgill.ca>
2022-02-26 16:30:08 -0500

    relativistic net force

--- Assets/Scripts/GravityData.cs
+++ Assets/Scripts/GravityData.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.Mathematics;
 using static Unity.Mathematics.math;
 using UnityEngine;
+using double3 = Unity.Mathematics.double3;
 
 public sealed class GravityData : MonoBehaviour
 {
+    private Rigidbody m_RigidBody;
+    private float3 m_PreviousVelocity;
+
     public IEnumerable<GameObject> allOtherGravityBodies => GameObject
         .FindGameObjectsWithTag("Gravity")
         .AsEnumerable()
@@ -18,15 +23,37 @@ public sealed class GravityData : MonoBehaviour
         UniversalGravitation(me.w, other.w, distance(me.xyz, other.xyz)) * normalize(other.xyz - me.xyz);
 
     public double3 UniversalGravitationWith(GameObject other) => UniversalGravitationBetween(
-        double4(double3(transform.position), GetComponent<Rigidbody>().mass),
+        double4(double3(transform.position), m_RigidBody.mass),
         double4(double3(other.transform.position), other.GetComponent<Rigidbody>().mass));
 
-    public double3 gravitationOnMe =>
+    public double3 gravitationOnMe { get; private set; }
+
+    public double3 calculatedGravitationOnMe =>
         allOtherGravityBodies.Aggregate(double3(0.0f), (acc, go) => acc + UniversalGravitationWith(go));
+
+    public double3 acceleration
+    {
+        get
+        {
+            float3 currentVel = m_RigidBody.velocity;
+            var acc = (currentVel - m_PreviousVelocity) / Time.fixedDeltaTime;
+            m_PreviousVelocity = currentVel;
+            return acc;
+        }
+    }
+
+    public double3 relativisticNetForce => acceleration * m_RigidBody.mass - gravitationOnMe;
+
+    private void Start()
+    {
+        m_RigidBody = GetComponent<Rigidbody>();
+        m_PreviousVelocity = m_RigidBody.velocity;
+        gravitationOnMe = double3.zero;
+    }
 
     private void FixedUpdate()
     {
-        var rb = GetComponent<Rigidbody>();
-        rb.AddForce(float3(gravitationOnMe));
+        gravitationOnMe = calculatedGravitationOnMe;
+        m_RigidBody.AddForce(float3(gravitationOnMe));
     }
 }
