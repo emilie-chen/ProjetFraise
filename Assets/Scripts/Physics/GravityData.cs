697ce98808667e7eda72cbcffe25b52a6dcf0fd9
Hongjun Chen <hongjun.chen2@mail.mcgill.ca>
2022-02-25 22:46:31 -0500

    Initial commit

---
+++ Assets/Scripts/Physics/GravityData.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEditor.Timeline.Actions;
+using static Unity.Mathematics.math;
+using UnityEngine;
+using UnityEngine.Jobs;
+
